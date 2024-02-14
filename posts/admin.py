4c6e605eb132d63242e4fe33416f092fa4352cad
khawla-Moh <khawlaMoh00@gmail.com>
2024-02-14 17:40:14 +0300

    create Posts Comments model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Posts,Comments,Category
 # Register your models here.
+
+class PostsAdmin(SummernoteModelAdmin):
+    list_display=['name','publish_date','category']
+    list_filter=['publish_date','category']
+
+
+
+admin.site.register(Posts,PostsAdmin)
+admin.site.register(Comments)
+admin.site.register(Category)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=16)
+    def __str__(self) :
+        return self.name
+
+
+class Posts(models.Model):
+    author=models.ForeignKey(User,related_name='post_user',on_delete=models.CASCADE)
+    name=models.CharField(max_length=40)
+    content=models.TextField(max_length=300)
+    publish_date=models.DateTimeField(auto_now_add=True)
+    category=models.ForeignKey(Category,related_name='post_category',on_delete=models.SET_NULL,null=True)
+    tags = TaggableManager() 
+    image=models.ImageField(upload_to='post')
+    
+
+    def __str__(self) :
+        return self.name 
+
+
+
+class Comments(models.Model):
+    post=models.ForeignKey(Posts,related_name='comments_post',on_delete=models.CASCADE)
+    user=models.ForeignKey(User,related_name='comments_user',on_delete=models.CASCADE)
+    text=models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self) :
+        return self.user
